bca46a95b4fa02f0f0b56c55a97e419cdb327e9c
beerappabvgp <beerappabharathb@gmail.com>
2026-06-16 14:06:59 +0530

    Happy Number

--- day2/HappyNumber.java
+++ day2/HappyNumber.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 
 public class HappyNumber {
 
+    // O(no_of_digits)
     public static int sumOfSquaresOfDigits(int n) {
         int sum = 0;
         while (n > 0) {
@@ -16,9 +17,11 @@ public class HappyNumber {
 
     public static boolean isHappy(int n) {
         HashSet<Integer> seen = new HashSet<>();
-
+        
+        // 0(1) * O(no_of_digits)
         while (n != 1) {
             if (seen.contains(n)) {
+                System.out.println("length of set: " + seen.size());
                 return false;
             }
             seen.add(n);
@@ -28,7 +31,7 @@ public class HappyNumber {
         return true;
     }
     public static void main(String[] args) {
-        int n = 199;
+        int n = 1999876995;
         if (isHappy(n)) {
             System.out.println("Happy Number ....");
         } else {
@@ -36,3 +39,6 @@ public class HappyNumber {
         }
     }
 }
+
+// OTC: O(1) * O(no_of_digits) = O(no_of_digits)
+// OSC: O(number of elements in the set)
